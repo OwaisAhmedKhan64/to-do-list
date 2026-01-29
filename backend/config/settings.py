a9d3c03c6a92a2e443df9d899f141f3307b5fb76
Owais Ahmed Khan <owaisahmedkhan64@gmail.com>
2026-01-29 10:43:18 +0500

    feat: Complete Vue.js frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # for Vue 
+    "http://127.0.0.1:8000", # for Django
+]
